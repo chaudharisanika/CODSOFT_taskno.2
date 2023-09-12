c9c8eeaef298926e1b3bb3b3fbd717eda330a36c
Sanika Chaudhari <sanikachaudhari201003@gmail.com>
2023-09-12 17:50:02 +0530

    done

--- PortFolio/script.js
+++ PortFolio/script.js
@@ -33,3 +33,61 @@ typewritern();
 // function setoldimage(){
 //     document.querySelector("#responsive").src="../Portfolio/image/p1.jpg"
 // }
+
+window.addEventListener('scroll', reveall);
+
+function reveall(){
+  var reveals = document.querySelectorAll('.reveall');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', revealr);
+function revealr(){
+  var reveals = document.querySelectorAll('.revealr');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reveal);
+function reveal(){
+  var reveals = document.querySelectorAll('.reveal');
+
+  for(var i = 0; i < reveals.length; i++){
+
+    var windowheight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowheight - revealpoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
